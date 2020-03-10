d2ac037873f86713b0d5dcdc843f56227858f860
vinvenea <s18291@pjwstk.edu.pl>
2020-03-10 09:56:51 +0100

    Ini

---
+++ Cw2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var path = @"C:\Users\vinve\Desktop\APBD\Zajęcia 2\dane.csv";
+            var lines = File.ReadLines(path);
+
+            foreach(var line in lines)
+            {
+                string[] dane = line.Split(',');
+                //Console.WriteLine(dane.Length);
+                foreach(var et in dane)
+                {
+                 if(et == "")
+                    {
+                        throw new Exception("Nope");
+                    }   
+                    
+                }
+            }
+            var today = DateTime.Today.ToShortDateString();
+        }
+    }
+}
